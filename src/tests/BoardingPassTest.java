420d6cfc4ce1ef70d9846564b0faff56a66b0d41
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2022-01-31 11:25:10 -0500

    initial commit

---
+++ src/tests/BoardingPassTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardingPassTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
