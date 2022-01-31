d81b26a18ed76beced94cd9b33978430040d92d1
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2022-01-31 11:47:42 -0500

    Update BoardingPass.java
    defined vars and generated getters and setters

--- src/java/BoardingPass.java
+++ src/java/BoardingPass.java
@@ -1,2 +1,78 @@
 public class BoardingPass {
+
+    // variables
+    private int boardingPassNumber;
+    private int date;
+    private int time;
+    private String origin;
+    private String destination;
+    private int eta;
+    private int departureTime;
+    private int ticketPrice;
+
+    // getters and setters
+
+    public int getBoardingPassNumber() {
+        return boardingPassNumber;
+    }
+
+    public void setBoardingPassNumber(int boardingPassNumber) {
+        this.boardingPassNumber = boardingPassNumber;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public int getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(int departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(int ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
 }
