086cc6fd4a21fa112686bb35b043c0582691a907
AbbaAdam1 <sonicdx15@gmail.com>
2022-02-01 13:23:44 -0500

    Updated WritetoFile.java

---
+++ src/tests/WritetoFileTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WritetoFileTest {
+    WritetoFile writeFile;
+
+    @BeforeEach
+    void setUp() {
+        writeFile = new WritetoFile();
+    }
+
+    @Test
+    void ticketInfo() {
+    }
+
+    @Test
+    void writeTicket() {
+    }
+}
