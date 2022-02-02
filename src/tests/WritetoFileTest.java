9af5594aa808b6f72bd7b7b3db400fbaadc95601
AbbaAdam1 <sonicdx15@gmail.com>
2022-02-02 16:22:29 -0500

    Update WritetoFileTest.java

--- src/tests/WritetoFileTest.java
+++ src/tests/WritetoFileTest.java
@@ -4,30 +4,62 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WritetoFileTest {
+    Customer customer;
+    BoardingPass boardingPass;
 
     @BeforeEach
     void setUp() {
+        Customer customer = new Customer();
+        BoardingPass boardingPass = new BoardingPass();
+
+        Date today;
+        today = new Date();
+        customer.setEmail("a@a.com");
+        customer.setName("abba");
+        customer.setGender("M");
+        customer.setAge(23);
+        customer.setNumber("1234567890");
+
+        boardingPass.setBoardingPassNumber(1234);
+        boardingPass.setOriginLocation(BoardingPass.Locations.Boston);
+        boardingPass.setDestinationLocation(BoardingPass.Locations.Atlanta);
+        boardingPass.setArrivalTime(today);
+        boardingPass.setTicketPrice(100.0);
+        //ETA
     }
 
     @Test
     void ticketInfo(@TempDir Path tempDir) {
         Path testingFolder = tempDir.resolve("ticketInfo.txt");
 
-        List<String> lines = Arrays.asList("1", "2", "3");
+        //List<String> lines = Arrays.asList("1", "2", "3");
+        String age = String.valueOf(customer.getAge());
+        String bpn = String.valueOf(boardingPass.getBoardingPassNumber());
+        String DL = String.valueOf(boardingPass.getDestinationLocation());
+        String OL = String.valueOf(boardingPass.getOriginLocation());
+        String DT = String.valueOf(boardingPass.getDepartureTime());
+        String AT = String.valueOf(boardingPass.getArrivalTime());
+        String ticketPrice = String.valueOf(boardingPass.getTicketPrice());
+        String ETA = String.valueOf(boardingPass.getEta());
+
+        List<String> test = Arrays.asList(customer.getName(), customer.getNumber(), customer.getGender(), age, bpn, DL, OL, DT, AT, ticketPrice, ETA);
         try {
-            Files.write(testingFolder, lines);
+            Files.write(testingFolder, test);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         assertAll(
                 () -> assertTrue(Files.exists(testingFolder), "File should exist"),
-                () -> assertLinesMatch(lines, Files.readAllLines(testingFolder)));
+                () -> assertLinesMatch(test, Files.readAllLines(testingFolder)));
     }
 
     @Test
