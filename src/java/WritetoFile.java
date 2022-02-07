14d443ded19d49f5bfe85f39b5f01dd721abc2e8
AbbaAdam1 <sonicdx15@gmail.com>
2022-02-07 16:19:08 -0500

    Update WritetoFile.java

--- src/java/WritetoFile.java
+++ src/java/WritetoFile.java
@@ -1,7 +1,4 @@
-import java.awt.*;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 
 import com.lowagie.text.*;
@@ -17,7 +14,6 @@ public class WritetoFile {
     public void ticketInfo(Customer customer, BoardingPass boardingPass) {
         {
             try {
-                //fw = new FileWriter("ticketInfo.txt");
                 File write = new File("ticketInfo.txt");
                 FileOutputStream fos = new FileOutputStream(write);
                 BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(fos));
@@ -61,121 +57,20 @@ public class WritetoFile {
             }
         }
     }
-
-    public void writeTicket(){
-        try {
-            FileReader fr = new FileReader("ticketInfo.txt");
-            FileWriter fw = new FileWriter("Ticket.txt");
-
-            Scanner myReader = new Scanner(fr);
-            String name = myReader.nextLine();
-            String email = myReader.nextLine();
-            String number = myReader.nextLine();
-            String gender = myReader.nextLine();
-            String age = myReader.nextLine();
-
-            String bpNumber = myReader.nextLine();
-            String destinationLocation = myReader.nextLine();
-            String originLocation = myReader.nextLine();
-            String departureTime = myReader.nextLine();
-            String arrivalTime = myReader.nextLine();
-            String ticketPrice = myReader.nextLine();
-            String ETA = myReader.nextLine();
-            myReader.close();
-
-            //Passenger info
-            fw.write("Passenger Information: ");
-            fw.write("Passenger Name:" + "          " + "Gender:" + "          " + "Age:");
-            fw.write(name + "          " + gender + "          " + age);
-            //space
-            fw.write("");
-
-            fw.write("Email:"+ "          " + "Phone Number:");
-            fw.write(email + "          " + number);
-
-            fw.write("Boarding Pass Number:");
-            fw.write(bpNumber);
-            //space
-            fw.write("");
-            fw.write("");
-
-            //Flight Information
-            fw.write("Travel Information:");
-            //space
-            fw.write("");
-
-            fw.write("From:" + "          " + "Departure Time:");
-            fw.write(originLocation + "          " + departureTime);
-            //space
-            fw.write("");
-
-            fw.write("To:" + "          " + "Arrival Time:");
-            fw.write(destinationLocation + "          " + arrivalTime);
-            //space
-            fw.write("");
-
-            fw.write("ETA:");
-            fw.write(ETA);
-            //space
-            fw.write("");
-            fw.write("");
-
-            fw.write("Fare:");
-            fw.write(ticketPrice);
-
-            fr.close();
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void pdf(BoardingPass boardingPass){
         try {
             FileReader fr = new FileReader("ticketInfo.txt");
             Scanner myReader = new Scanner(fr);
-            /*
-            String name = myReader.nextLine();
-            Paragraph PName = new Paragraph(name);
-            String email = myReader.nextLine();
-            Paragraph PEmail = new Paragraph(email);
-            String number = myReader.nextLine();
-            Paragraph PNumber = new Paragraph(number);
-            String gender = myReader.nextLine();
-            Paragraph PGender = new Paragraph(gender);
-            String age = myReader.nextLine();
-            Paragraph PAge = new Paragraph(age);
 
-            String bpNumber = myReader.nextLine();
-            Paragraph PBPNumber = new Paragraph(bpNumber);
-            String destinationLocation = myReader.nextLine();
-            Paragraph PDestinationLocation = new Paragraph(destinationLocation);
-            String originLocation = myReader.nextLine();
-            Paragraph PoriginLocation = new Paragraph(originLocation);
-            String departureTime = myReader.nextLine();
-            Paragraph PdepartureTime = new Paragraph(departureTime);
-            String arrivalTime = myReader.nextLine();
-            Paragraph ParrivalTime = new Paragraph(arrivalTime);
-            String ticketPrice = myReader.nextLine();
-            Paragraph PticketPrice = new Paragraph(ticketPrice);
-            //String ETA = myReader.nextLine();
-            //Paragraph PETA = new Paragraph(ETA);
-
-
-            myReader.close();
-*/
             BarcodePDF417 pdf417 = new BarcodePDF417();
             Document document = new Document(PageSize.A4, 50, 50, 50, 50);
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Ticket.pdf"));
-            //writer.
             document.open();
             pdf417.setText(String.valueOf(boardingPass.getBoardingPassNumber()));
             Image img = pdf417.getImage();
             img.scalePercent(300, 300 * pdf417.getYHeight());
-            //document.addTitle("Ticket");
             FontSelector selector = new FontSelector();
             Font f1 = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 30);
-            //f1.setColor(Color.blue);
             selector.addFont(f1);
             Phrase ph = selector.process("Ticket Information");
             Paragraph title = new Paragraph(ph);
@@ -186,10 +81,6 @@ public class WritetoFile {
             String line;
             Paragraph PLine = new Paragraph(ph);
 
-            //line = " ";
-            //PLine = new Paragraph(line);
-            //document.add(PLine);
-            //document.add(PLine);
             document.add(Chunk.NEWLINE);
             document.add(Chunk.NEWLINE);
             document.add(Chunk.NEWLINE);
@@ -198,7 +89,6 @@ public class WritetoFile {
                 switch (i){
                     case 1:
                         f1.setSize(13);
-                        //f1.setColor(Color.BLACK);
                         selector.addFont(f1);
                         ph = selector.process("Passenger Information: ");
                         PLine = new Paragraph(ph);
@@ -270,35 +160,15 @@ public class WritetoFile {
                         document.add(PLine);
                         break;
                 }
-
                 line = myReader.nextLine();
                 Paragraph paragraph = new Paragraph(line);
-                //paragraph.setAlignment("center");
                 paragraph.setIndentationLeft(100.0f);
                 document.add(paragraph);
                 i++;
             }
             myReader.close();
-
-            /*
-            //document.add(PEmail);
-            document.add(PNumber);
-            document.add(PGender);
-            document.add(PAge);
-
-            document.add(PBPNumber);
-            document.add(PDestinationLocation);
-            document.add(PoriginLocation);
-            document.add(PdepartureTime);
-            document.add(ParrivalTime);
-            document.add(PticketPrice);
-            //document.add(PETA);
-
-             */
             document.add(Chunk.NEWLINE);
             document.add(img);
-
-
             document.close();
         } catch (DocumentException | FileNotFoundException e) {
             e.printStackTrace();
